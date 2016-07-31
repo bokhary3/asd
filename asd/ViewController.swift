c07a283bf86f90c8a298060a377e01ec12b928c6
elsayed.hussein13@gmail.com <hosny@192.168.1.6>
2016-08-01 00:49:21 +0200

    this is ios

---
+++ asd/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  asd
+//
+//  Created by Elsayed Hussein on 6/24/16.
+//  Copyright © 2016 Elsayed Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        let s = "sss"
+        
+        
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
